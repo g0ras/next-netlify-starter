f1c9f27e70094ea2b664fdffc029b3558415bc05
g0ras <160677918+g0ras@users.noreply.github.com>
2024-02-21 23:33:26 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8888/',
+    baseUrl: 'https://next-jay-ess.netlify.app/',
     supportFile: false,
   },
 });
